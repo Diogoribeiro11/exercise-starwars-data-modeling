a3bbfcd6e9051d22bbab7434acd08fe694b6f285
Diogoribeiro11 <110627684+Diogoribeiro11@users.noreply.github.com>
2022-11-21 16:01:06 +0000

    Update models.py

--- src/models.py
+++ src/models.py
@@ -8,18 +8,43 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(base):
+    __tablename__ = 'user'
+    user_name = Column(String(20), primary_key=True)
+    password = Column(String(15))
+    person_id = Column(Integer, ForeignKey('person.id'))
+    person = relationship(Person)
+    character_id = Column(Integer, ForeignKey('character.id'))
+    character = relationship(character)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+    
+
+class Character(base):
+    __tablename__ = 'character'
+    character_id = Column(Integer, primary_key=True)
+    character_name = Column(String(15))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    planet = relationship(Planet)
+
+
+class Planet(base):
+    __tablename__ = 'planet'
+    planet_id = Column(Integer, primary_key=True)
+    planet_name = Column(String(15))
+
 class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    person_id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
 class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    address_id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
